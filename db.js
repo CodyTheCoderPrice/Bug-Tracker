30b9d6b43f5f9ac46e984fbd4f406399174a96a9
Cody Price <CodyWilliamPrice@outlook.com>
2020-12-21 18:31:42 -0500

    Returned to used http on the backend due to bug with https and heroku

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ const env = process.env.NODE_ENV || "development";
 let connectionString = {
 	connectionString:
 		process.env.DATABASE_URL,
-	ssl: true,
+	ssl: false,
 };
 
 /* connectionString.connectionString =
--- server.js
+++ server.js
@@ -2,16 +2,14 @@
 const express = require("express");
 const app = express();
 // Http & https
-const fs = require("fs");
-//const http = require("http");
+/* const fs = require("fs");
+const http = require("http");
 const https = require("https");
-//const httpPort = 8080;
+const httpPort = 8080;
 const httpsPort = process.env.PORT || 8443;
 const privateKey = fs.readFileSync("./sslCertification/selfsigned.key", "utf8");
 const certificate = fs.readFileSync("./sslCertification/selfsigned.crt", "utf8");
-const credentials = { key: privateKey, cert: certificate };
-// URL
-const url = require("url");
+const credentials = { key: privateKey, cert: certificate }; */
 // Routes
 const priorityStatus = require("./routes/priorityStatus");
 const accounts = require("./routes/accounts");
@@ -21,32 +19,14 @@ const comments = require("./routes/comments");
 
 // Middleware
 app.use(express.json());
-/* app.use(function (req, res, next) {
-	if (req.secure) {
-		// Request was via https, so do no special handling
-		console.log(
-			"Was secure --> " +
-				url.format({
-					protocol: req.protocol,
-					host: req.get("host"),
-					pathname: req.originalUrl,
-				})
-		);
-		next();
-	} else {
-		// Request was via http, so redirect to https
-		console.log("Switched to https");
-		next();
-		res.redirect("https://" + req.headers.host + req.url);
-	}
-}); */
 
-app.all('*', function(req, res, next) {
+// Was being used when trying to use HTTPS
+/* app.all('*', function(req, res, next) {
     if (req.headers['x-forwarded-proto'] != 'https')
         res.redirect('https://' + req.headers.host + req.url)
     else
         next()
-});
+}); */
 
 if (process.env.NODE_ENV === 'production') {
 	app.use(express.static('client/build'));
@@ -63,13 +43,12 @@ app.use("/api/project", projects);
 app.use("/api/bug", bugs);
 app.use("/api/comment", comments);
 
-//const httpServer = http.createServer(app);
+// Was being used when trying to use HTTPS
+/* const httpServer = http.createServer(app);
 const httpsServer = https.createServer(credentials, app);
+httpServer.listen(httpPort);
+httpsServer.listen(httpsPort); */
 
-//httpServer.listen(httpPort);
-httpsServer.listen(httpsPort);
-
-// Was using before switching to https
-/* app.listen(process.env.PORT || 5000, () => {
+app.listen(process.env.PORT || 5000, () => {
 	console.log("Server has started on port 5000");
-}); */
+});
