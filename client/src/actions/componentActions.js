9fc471e1d565f46fdbe208dd1f84b33bfe286f95
Cody Price <CodyWilliamPrice@outlook.com>
2021-01-05 14:05:00 -0500

    Documented componentActions with jsdoc

--- client/src/actions/componentActions.js
+++ client/src/actions/componentActions.js
@@ -1,3 +1,4 @@
+// Redux containers
 import {
 	GENERAL_CONTAINER,
 	ACCOUNT_CONTAINER,
@@ -5,6 +6,7 @@ import {
 	BUG_CONTAINER,
 	COMMENT_CONTAINER,
 } from "./constants/containerNames";
+// Redux types
 import {
 	SET_WHICH_GENERAL_COMPONENTS_DISPLAY,
 	SET_WHICH_LIST_COMPONENTS_DISPLAY,
@@ -12,6 +14,13 @@ import {
 	SET_WHICH_COMMENT_COMPONENTS_DISPLAY,
 } from "./constants/types";
 
+/**
+ * Sets JSON object in general container of the redux state for how general
+ * components should be displayed in the app
+ * 
+ * @param {JSON} displays - JSON containing info for how general components
+ * should be displyed in the app
+ */
 export const setWhichGeneralComponentsDisplay = (displays) => (dispatch) => {
 	dispatch({
 		container: GENERAL_CONTAINER,
@@ -20,6 +29,13 @@ export const setWhichGeneralComponentsDisplay = (displays) => (dispatch) => {
 	});
 };
 
+/**
+ * Sets JSON object in account container of the redux state for how account
+ * components should be displayed in the app
+ * 
+ * @param {JSON} displays - JSON containing info for how account components
+ * should be displyed in the app
+ */
 export const setWhichAccountComponentsDisplay = (displays) => (dispatch) => {
 	dispatch({
 		container: ACCOUNT_CONTAINER,
@@ -28,6 +44,13 @@ export const setWhichAccountComponentsDisplay = (displays) => (dispatch) => {
 	});
 };
 
+/**
+ * Sets JSON object in project container of the redux state for how project
+ * components should be displayed in the app
+ * 
+ * @param {JSON} displays - JSON containing info for how project components
+ * should be displyed in the app
+ */
 export const setWhichProjectComponentsDisplay = (displays) => (dispatch) => {
 	dispatch({
 		container: PROJECT_CONTAINER,
@@ -36,6 +59,13 @@ export const setWhichProjectComponentsDisplay = (displays) => (dispatch) => {
 	});
 };
 
+/**
+ * Sets JSON object in bug container of the redux state for how bug
+ * components should be displayed in the app
+ * 
+ * @param {JSON} displays - JSON containing info for how bug components
+ * should be displyed in the app
+ */
 export const setWhichBugComponentsDisplay = (displays) => (dispatch) => {
 	dispatch({
 		container: BUG_CONTAINER,
@@ -44,6 +74,13 @@ export const setWhichBugComponentsDisplay = (displays) => (dispatch) => {
 	});
 };
 
+/**
+ * Sets JSON object in comment container of the redux state for how comment
+ * components should be displayed in the app
+ * 
+ * @param {JSON} displays - JSON containing info for how comment components
+ * should be displyed in the app
+ */
 export const setWhichCommentComponentsDisplay = (displays) => (dispatch) => {
 	dispatch({
 		container: COMMENT_CONTAINER,
