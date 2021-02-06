1329e799643a425946fab00b07bb39d6e9338887
Cody Price <CodyWilliamPrice@outlook.com>
2021-02-06 13:17:16 -0500

    Slight redesign to account modal and refactored blurred backgrounds

--- client/src/components/home/account/AccountModal.js
+++ client/src/components/home/account/AccountModal.js
@@ -22,35 +22,29 @@ export default function AccountModal() {
 	return (
 		<div className="edit-account-modal-component">
 			<div className="edit-account-modal">
-				<div className="inner-border-padding-container">
-					<div className="exit-button" onClick={backToAccountSidebar}>
-						<i
-							className="fa fa-times"
-							aria-hidden="true"
-							alt="icon of an X"
-						></i>
-					</div>
-					{reduxState[ACCOUNT_CONTAINER].componentsDisplay
-						.accountModalChangeInfo ? (
-						<AccountModalChangeInfo />
-					) : null}
-					{reduxState[ACCOUNT_CONTAINER].componentsDisplay
-						.accountModalChangeEmail ? (
-						<AccountModalChangeEmail />
-					) : null}
-					{reduxState[ACCOUNT_CONTAINER].componentsDisplay
-						.accountModalChangePassword ? (
-						<AccountModalChangePassword />
-					) : null}
-					{reduxState[ACCOUNT_CONTAINER].componentsDisplay
-						.accountModalDeleteAccount ? (
-						<AccountModalDeleteAccount />
-					) : null}
-					{reduxState[ACCOUNT_CONTAINER].componentsDisplay
-						.accountModalChangeSettings ? (
-						<AccountModalChangeSettings />
-					) : null}
+				<div className="exit-button" onClick={backToAccountSidebar}>
+					<i className="fa fa-times" aria-hidden="true" alt="icon of an X"></i>
 				</div>
+				{reduxState[ACCOUNT_CONTAINER].componentsDisplay
+					.accountModalChangeInfo ? (
+					<AccountModalChangeInfo />
+				) : null}
+				{reduxState[ACCOUNT_CONTAINER].componentsDisplay
+					.accountModalChangeEmail ? (
+					<AccountModalChangeEmail />
+				) : null}
+				{reduxState[ACCOUNT_CONTAINER].componentsDisplay
+					.accountModalChangePassword ? (
+					<AccountModalChangePassword />
+				) : null}
+				{reduxState[ACCOUNT_CONTAINER].componentsDisplay
+					.accountModalDeleteAccount ? (
+					<AccountModalDeleteAccount />
+				) : null}
+				{reduxState[ACCOUNT_CONTAINER].componentsDisplay
+					.accountModalChangeSettings ? (
+					<AccountModalChangeSettings />
+				) : null}
 			</div>
 		</div>
 	);
