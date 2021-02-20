3b008da0ba647f8a7cb632201b84904dd5bba86c
Cody Price <CodyWilliamPrice@outlook.com>
2021-02-20 15:59:46 -0500

    Redesigned ItemViewBugListRow overflow to be much more simple

--- client/src/components/home/projects-bugs-shared/item/ItemViewBugListRow.js
+++ client/src/components/home/projects-bugs-shared/item/ItemViewBugListRow.js
@@ -1,36 +1,12 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { useSelector } from "react-redux";
 
 import { ACCOUNT_CONTAINER } from "../../../../actions/constants/containerNames";
 
-import {
-	getElementSize,
-	getBaseSecondaryTextColorClassNameForLightOrDarkMode,
-} from "../../../../utils";
+import { getBaseSecondaryTextColorClassNameForLightOrDarkMode } from "../../../../utils";
 
 export default function ItemViewBugListRow(props) {
 	const reduxState = useSelector((state) => state);
-
-	useEffect(() => {
-		const tableDataElement = document.getElementsByClassName(
-			"js-bug-name-table-data"
-		)[0];
-
-		const overflowContainerElement = document.getElementsByClassName(
-			"js-bug-name-overflow-container-" + props.item.id
-		)[0];
-
-		const myObserver = new ResizeObserver(() => {
-			const tableDataWidth = getElementSize(tableDataElement).width;
-
-			// Minus 20 to act as a padding on the left and right side
-			overflowContainerElement.style.width = tableDataWidth - 20 + "px";
-			overflowContainerElement.style.maxWidth = tableDataWidth - 20 + "px";
-		});
-
-		myObserver.observe(tableDataElement);
-		// eslint-disable-next-line
-	}, []);
 
 	const getStatusBoxColorClassName = () => {
 		const filteredStatusList = reduxState[
@@ -54,30 +30,27 @@ export default function ItemViewBugListRow(props) {
 				)
 			}
 		>
-			<td className="bug-list-table__data js-bug-name-table-data">
-				<div
-					className={
-						"bug-list-table__data__overflow-container js-bug-name-overflow-container-" +
-						props.item.id +
-						(props.item.status_id ===
-						reduxState[props.reduxContainerName].priorityStatusOptions
-							.statusCompletionId
-							? " bug-list-table__data__overflow-container--completed-color"
-							: "")
-					}
-				>
-					<span className="bug-list-table__data__overflow-container__info">
-						{props.item.status_id !==
-						reduxState[props.reduxContainerName].priorityStatusOptions
-							.statusCompletionId ? null : (
-							<i
-								className="fa fa-check bug-list-table__data__overflow-container__info__completed-icon"
-								aria-hidden="true"
-							/>
-						)}
-						{props.item.name}
-					</span>
-				</div>
+			<td
+				className={
+					"bug-list-table__data bug-list-table__data--overflow" +
+					(props.item.status_id ===
+					reduxState[props.reduxContainerName].priorityStatusOptions
+						.statusCompletionId
+						? " bug-list-table__data--completed-color"
+						: "")
+				}
+			>
+				<span className="bug-list-table__data__info">
+					{props.item.status_id !==
+					reduxState[props.reduxContainerName].priorityStatusOptions
+						.statusCompletionId ? null : (
+						<i
+							className="fa fa-check bug-list-table__data__info__completed-icon"
+							aria-hidden="true"
+						/>
+					)}
+					{props.item.name}
+				</span>
 			</td>
 			<td className="bug-list-table__data">
 				<div className="bug-list-table__data__centering-container">
