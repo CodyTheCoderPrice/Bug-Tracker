9a05d27641807cb1d4ba176b6f456d23e402ef9d
Cody Price <CodyWilliamPrice@outlook.com>
2021-04-06 13:48:26 -0400

    Added JsDoc to bugStatisticsUtils

--- client/src/utils/bugStatisticsUtils.js
+++ client/src/utils/bugStatisticsUtils.js
@@ -1,18 +1,37 @@
+// This util imports container names as it works with the redux state
 import {
 	BUG_CONTAINER,
 } from "../actions/constants/containerNames";
 
+/**
+ * Gets the number of bugs within a project that have a particular status
+ * 
+ * @param {JSON} passedReduxState - Current redux state from useSelector 
+ * @param {Number} projectId - Id of the project that the bugs belong to
+ * @param {NUMBER} statusId - Id of the status option in question for finding
+ * the number of bugs for
+ * @returns The number of bugs within a project that have a particular status
+ */
 export function getNumberOfBugsForStatus(
 	passedReduxState,
 	projectId,
 	statusId
 ) {
+	// Spread operator makes deep copy of bug list so original is not affected
 	return [...passedReduxState[BUG_CONTAINER].list].filter(
 		(item) => item.project_id === projectId && item.status_id === statusId
 	).length;
 }
 
+/**
+ * Gets a list of all bugs for a particular project
+ * 
+ * @param {JSON} passedReduxState - Current redux state from useSelector
+ * @param {Number} projectId - Id of the project thats bugs are to be retrieved
+ * @returns List of bugs for a particular project
+ */
 export function getBugsInProjectList(passedReduxState, projectId) {
+	// Spread operator makes deep copy of bug list so original is not affected
 	return [...passedReduxState[BUG_CONTAINER].list].filter(
 		(item) => item.project_id === projectId
 	);
