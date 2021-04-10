7697dd5edb55e251d95af8a2365e9404077f812c
Cody Price <CodyWilliamPrice@outlook.com>
2021-04-10 15:00:36 -0400

    Refactored and added JsDoc to dateUtils

--- client/src/utils/dateUtils.js
+++ client/src/utils/dateUtils.js
@@ -1,14 +1,40 @@
 // Easier to use than Date()
 import moment from "moment";
 
+import {
+	isEmpty,
+} from "./index";
+
+
+/**
+ * Format date in YYYY-MM-DD, or an empty string if date is empty
+ * 
+ * @param {(Date|String)} date - Date (type date or string) to be formatted
+ * @returns {String} Either the date formated in YYYY-MM-DD, or an empty string
+ * if date is empty
+ */
 export function formatDateYYYYmmDD(date) {
-	return (date !== null) ? moment.utc(date).format('YYYY-MM-DD') : "";
+	return (!isEmpty(date)) ? moment.utc(date).format('YYYY-MM-DD') : "";
 }
 
+/**
+ * Format date in MM-DD-YYYY, or an empty string if date is empty
+ * 
+ * @param {(Date|String)} date - Date (type date or string) to be formatted
+ * @returns {String} Either the date formated in MM-DD-YYYY, or an empty string
+ * if date is empty
+ */
 export function formatDateMMddYYYY(date) {
-	return (date !== null) ? moment.utc(date).format('MM-DD-YYYY') : "";
+	return (!isEmpty(date)) ? moment.utc(date).format('MM-DD-YYYY') : "";
 }
 
+/**
+ * Converts date to an number
+ * 
+ * @param {(Date|String)} date - Date (type date or string) to be formatted
+ * @returns {Number} Date converted to a number
+ */
 export function dateToInt(date) {
-	return (date !== null) ? Number(moment.utc(date)) : 0;
+	console.log(typeof moment());
+	return (!isEmpty(date)) ? Number(moment.utc(date)) : 0;
 }
