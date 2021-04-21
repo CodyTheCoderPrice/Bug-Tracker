8d1705772042aaf81bf4d3a4c2c0bf1367aa5b00
Cody Price <CodyWilliamPrice@outlook.com>
2021-04-21 16:17:47 -0400

    Added JsDoc to stringUtils

--- client/src/utils/stringUtils.js
+++ client/src/utils/stringUtils.js
@@ -1,7 +1,22 @@
-export function stripNonDigits(stringValue) {
-	return Number(stringValue.replace(/[^\d.-]/g, ""));
+
+/**
+ * Removes all non-digits from a string, and then returns it as a number. If 
+ * string does not have any digits, then the number 0 is returned.
+ * 
+ * @param {String} str - Any string
+ * @returns {Number} String parameter with all non-digits removed and converted
+ * to a number
+ */
+export function stripNonDigits(str) {
+	return Number(str.replace(/[^\d.-]/g, ""));
 }
 
+/**
+ * Capitalizes the first letter of each word inside a string, then returns it
+ * 
+ * @param {String} str - Any string 
+ * @returns String parameter with the first letter of each word capitalized
+ */
 export function capitalizeFistLetterOfEachWord(str) {
 	return str.toLowerCase().replace(/\w\S*/g, (txt) => {
 		return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
