999d707fcb557919e2f8d498e2e54a0a0738fd63
Cody Price <CodyWilliamPrice@outlook.com>
2021-04-22 18:35:37 -0400

    Deleted un-needed utils file

--- client/src/utils/hooks/index.js
+++ client/src/utils/hooks/index.js
@@ -1,5 +1,4 @@
 // Exports all hook utils
-export * from "./descriptionTextAreaHookUtils";
 export * from "./perserveDateInputHookUtils";
 export * from "./searchBarHookUtils";
 export * from "./sidebarResizeHookUtils";
