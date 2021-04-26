59589a439f722141eb09f35863b9c8ccf7847c5f
Cody Price <CodyWilliamPrice@outlook.com>
2021-04-26 13:53:28 -0400

    Added JsDoc to store.js

--- client/src/store.js
+++ client/src/store.js
@@ -24,45 +24,47 @@ const initialState = {};
 
 const middleware = [thunk];
 
-// Used to persist redux at the root level
+// Root level persistConfig
 const rootPersistConfig = {
 	key: "root",
 	storage: storage,
 	whitelist: [
-		// Persisted despite account info and comments being re-fetched on
-		// ...page refresh so it is not null while data is retrieved from server
+		// Persisted despite account info and comments being re-fetched on page
+		// ...refresh, so they're not null while data is re-fetched from server
 		ACCOUNT_CONTAINER,
 		COMMENT_CONTAINER,
 	],
 };
 
-//==============================================================================
-// Following persistConfigs are used for nested redux persist, one level deeper
-//==============================================================================
+// Nested persistConfigs (one level deeper than root)
 const generalComponentsPersistConfig = {
 	key: GENERAL_CONTAINER,
 	storage: storage,
 	whitelist: ["componentsDisplay"],
 };
 
+// Nested persistConfigs (one level deeper than root)
 const projectContainerPersistConfig = {
 	key: PROJECT_CONTAINER,
 	storage: storage,
-	// Projects and priorityStatusOptions are persisted despite being re-fetched
-	// ...on page refresh so they are not null while data is retrieved from server
+	// Projects and priorityStatusOptions are persisted despite being 
+	// ...re-fetched on page refresh, so they're not null while data is 
+	// ...re-fetched from server
 	blacklist: ["searchFilterSort"],
 };
 
+// Nested persistConfigs (one level deeper than root)
 const bugContainerPersistConfig = {
 	key: BUG_CONTAINER,
 	storage: storage,
-	// Projects and priorityStatusOptions are persisted despite being re-fetched
-	// ...on page refresh so they are not null while data is retrieved from server
+	// Projects and priorityStatusOptions are persisted despite being 
+	// ...re-fetched on page refresh, so they're not null while data is 
+	// ...re-fetched from server
 	blacklist: ["searchFilterSort"],
 };
 
-// rootReducer is created here instead of in the reducer index.js
-// ...so nested persist can be used
+// rootReducer is created here instead of in the reducers/index.js so nested
+// ...persist can be used
 const rootReducer = combineReducers({
 	[GENERAL_CONTAINER]: persistReducer(
 		generalComponentsPersistConfig,
