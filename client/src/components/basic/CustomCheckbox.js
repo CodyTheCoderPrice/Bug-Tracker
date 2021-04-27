60ef5bae689fed2eeb7197fb25f6af3df794b54a
Cody Price <CodyWilliamPrice@outlook.com>
2021-04-27 18:32:13 -0400

    Refactored JsDoc for CustomeCheckbox element

--- client/src/components/basic/CustomCheckbox.js
+++ client/src/components/basic/CustomCheckbox.js
@@ -5,29 +5,18 @@ import { getCustomCheckboxBorderBackgroundTextColorClassNameForThemeWithLightOrD
 /**
  * React functional component for a custom checkbox element with border,
  * background, and text colors specific to the theme + light/dark mode.
- *
+ * Component needs several properties passed in order to work. An example of
+ * those properties is below.
+ * 
  * @component
  * @example
- * name = "statusFilter"
- * value = reduxState[props.reduxContainerName].priorityStatusOptions
- * 		.statusList[0].id
- * onChangeFunction=(e) => {
- *		dispatch(
- *			setProjectOrBugSearchFilterSort(props.reduxContainerName, {
- *				...reduxState[props.reduxContainerName].searchFilterSort,
- *				[e.target.name]: getUpdatedDeepCopyFilterArray(
- *					reduxState,
- *					props.reduxContainerName,
- *					e.target.name,
- *					e.target.value
- *				),
- *			})
- *		);
- * id = "list-status-filter-" + reduxState[props.reduxContainerName]
- * 			.priorityStatusOptions.statusList[0].id
- * isChecked = !reduxState[props.reduxContainerName].searchFilterSort
- * 		.statusFilter.includes(reduxState[props.reduxContainerName]
- * 			.priorityStatusOptions.statusList[0].id)
+ * name = "nameHere"
+ * value = 1
+ * onChangeFunction=(e) => { 
+ * 	// Do something
+ * }
+ * id = "idHere"
+ * isChecked = booleanVariable
  * dark_mode = reduxState[ACCOUNT_CONTAINER].settings.dark_mode
  * theme_color = reduxState[ACCOUNT_CONTAINER].settings.theme_color
  */
@@ -67,15 +56,15 @@ export default function CustomCheckbox(props) {
 }
 
 CustomCheckbox.propTypes = {
-	// Value for checkbox input element's name attribute
+	// Checkbox input element's name attribute
 	name: PropTypes.string.isRequired,
-	// Value for checkbox input element's value attribute
+	// Checkbox input element's value attribute
 	value: PropTypes.oneOfType([PropTypes.number, PropTypes.string]).isRequired,
-	// Value for checkbox input element's onChange attribute
+	// Checkbox input element's onChange attribute
 	onChangeFunction: PropTypes.func.isRequired,
-	// Value for checkbox input element's checked attribute
+	// Checkbox input element's checked attribute
 	isChecked: PropTypes.bool.isRequired,
-	// Value for checkbox input element's id attribute
+	// Checkbox input element's id attribute
 	id: PropTypes.string,
 	// Whether or not the app is in dark mode
 	dark_mode: PropTypes.bool.isRequired,
