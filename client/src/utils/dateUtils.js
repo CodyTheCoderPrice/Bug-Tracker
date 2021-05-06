aa25a236ee817309d7791c15745d988ed7b9c60c
Cody Price <CodyWilliamPrice@outlook.com>
2021-05-06 19:18:55 -0400

    Refactored JsDoc for dateUtils to be more descriptive

--- client/src/utils/dateUtils.js
+++ client/src/utils/dateUtils.js
@@ -12,9 +12,13 @@ import {
  * @param {(Date|string)} date - Date (type date or string) to be formatted
  * @returns {string} Either the date formated in YYYY-MM-DD, or an empty string
  * if date is empty
+ * 
+ * @example
+ * // Returns "2019-12-05"
+ * formatDateYYYYmmDD("2019-12-05T05:00:00.000Z");
  */
 export function formatDateYYYYmmDD(date) {
-	return (!isEmpty(date)) ? moment.utc(date).format('YYYY-MM-DD') : "";
+	return !isEmpty(date) ? moment.utc(date).format('YYYY-MM-DD') : "";
 }
 
 /**
@@ -23,9 +27,13 @@ export function formatDateYYYYmmDD(date) {
  * @param {(Date|string)} date - Date (type date or string) to be formatted
  * @returns {string} Either the date formated in MM-DD-YYYY, or an empty string
  * if date is empty
+ * 
+ * @example
+ * // Returns "12-05-2019"
+ * formatDateMMddYYYY("2019-12-05T05:00:00.000Z");
  */
 export function formatDateMMddYYYY(date) {
-	return (!isEmpty(date)) ? moment.utc(date).format('MM-DD-YYYY') : "";
+	return !isEmpty(date) ? moment.utc(date).format('MM-DD-YYYY') : "";
 }
 
 /**
@@ -33,7 +41,11 @@ export function formatDateMMddYYYY(date) {
  * 
  * @param {(Date|string)} date - Date (type date or string) to be formatted
  * @returns {number} Date converted to a number
+ * 
+ * @example
+ * // Returns 1578200400000
+ * dateToInt("2020-01-05T05:00:00.000Z");
  */
 export function dateToInt(date) {
-	return (!isEmpty(date)) ? Number(moment.utc(date)) : 0;
+	return !isEmpty(date) ? Number(moment.utc(date)) : 0;
 }
